GoogleCloudPlatform/pontem

commit acecee25f7b91944fe55c6c0026533b2cd94b259

Making a few small changes to fix typos.

src/main/java/com/google/cloud/pontem/CloudSpannerDatabaseBackup.java
@@ -359,8 +359,8 @@ public class CloudSpannerDatabaseBackup {
     ImmutableList<Struct> tableNames =
         util.performSingleSpannerQuery(projectId, instance, databaseId, tableNamesQuery);
     ArrayList<String> tableNamesAsStrings = new ArrayList<String>();
+    String parentTableName = "";
     for (Struct inputRow : tableNames) {
-      String parentTableName = "";
       if (!inputRow.isNull("parent_table_name")) {
         parentTableName = inputRow.getString("parent_table_name");
       }

pom.xml
@@ -31,7 +31,7 @@
 
     <properties>
         <!-- Since Cloud Spanner is using GAX 1.1.0, must use Cloud Storage version
-             with the same version of GAX, which goes all the wa back to 1.0.2 -->
+             with the same version of GAX, which goes all the way back to 1.0.2 -->
         <google-cloud-storage.version>1.0.2</google-cloud-storage.version>
         <java.version>1.8</java.version>
     </properties>

run_all_tests.sh
@@ -83,7 +83,7 @@ mvn compile exec:java \
 echo "FINISHED setup phase."
 
 ## Backup
-echo "BEGIN bacup."
+echo "BEGIN backup."
 mvn clean compile exec:java  -Dexec.mainClass=com.google.cloud.pontem.CloudSpannerDatabaseBackup  -Dexec.args="--runner=DataflowRunner \
  --project=${GCP_PROJECT} \
  --gcpTempLocation=gs://${GCP_BUCKET}/tmp \

commit c59242f4108f91507e3f42e3467522634edcbb1b

Fix small bug introduced in commit acecee25f7b91944fe55c6c0026533b2cd94b259

src/main/java/com/google/cloud/pontem/CloudSpannerDatabaseBackup.java
@@ -359,8 +359,8 @@ public class CloudSpannerDatabaseBackup {
     ImmutableList<Struct> tableNames =
         util.performSingleSpannerQuery(projectId, instance, databaseId, tableNamesQuery);
     ArrayList<String> tableNamesAsStrings = new ArrayList<String>();
-    String parentTableName = "";
     for (Struct inputRow : tableNames) {
+      String parentTableName = "";
       if (!inputRow.isNull("parent_table_name")) {
         parentTableName = inputRow.getString("parent_table_name");
       }

src/main/java/com/google/cloud/pontem/CloudSpannerDatabaseRestore.java
@@ -366,6 +366,11 @@ public class CloudSpannerDatabaseRestore {
             options.getTablesToExcludeFromRestore(),
             util);
 
+    if (mapOfParentToAllChildrenTablesInOrderToFetch.size() == 0) {
+      LOG.info("Attempting to restore backup for 0 tables."
+          + "Input Folder: " + options.getInputFolder());
+    }
+
     // STEP 4: Create Pipeline, Read data from disk, Convert to Mutation, and Write to Cloud
     // Spanner.
     // For each root table, create a thread that handles that table and all its children.