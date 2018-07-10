b359d733d151e0f307540c8376d689e5b474f73d
Eric Beach <ebeach@google.com>
2018-07-10 09:56:14 -0400

    Bump Pontem version

--- src/main/java/com/google/cloud/pontem/Util.java
+++ src/main/java/com/google/cloud/pontem/Util.java
@@ -48,7 +48,7 @@ public class Util {
       "metadata/database_table_names.txt";
   public static final String FILE_PATH_FOR_TABLE_NAMES_ROW_COUNTS_FROM_JOB_METRICS =
       "metadata/table_row_counts_from_backup_job_metrics.txt";
-  public static final String USER_AGENT_PREFIX = "pontem/0.0.1";
+  public static final String USER_AGENT_PREFIX = "pontem/0.0.3";
 
   // Use a delimeter that cannot be a part of a Cloud Spanner table name.
   public static final String TRANSFORM_NODE_NAME_DELIMITER = "-#-";
