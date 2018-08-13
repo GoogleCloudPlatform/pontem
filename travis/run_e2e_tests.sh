15bc93953ff89350aadcf28f3bb05b7248fbeca6
Lucas Daniel <lmadariaga@google.com>
2018-08-13 12:43:06 -0300

    Updating script parameters and using them as part of the success text

--- travis/run_e2e_tests.sh
+++ travis/run_e2e_tests.sh
@@ -80,14 +80,14 @@ echo "Database Name Set: ${DATABASE_NAME}"
 GCP_FOLDER="my-database-"${DATE_STR}
 echo "GCP Folder Name Set: ${GCP_FOLDER}"
 
-if [ "${TEST_SUITE}" = "SERIAL" ]; then
+if [ "${TEST_SUITE}" = "Serialized" ]; then
   ./travis/run_serialized_e2e_tests.sh --project="${GCP_PROJECT}" --bucket="${GCP_BUCKET}" --database="${DATABASE_INSTANCE}" 
-elif [ "${TEST_SUITE}" = "AVRO" ]; then 
+elif [ "${TEST_SUITE}" = "Avro" ]; then 
   ./travis/run_avro_e2e_tests.sh --project="${GCP_PROJECT}" --bucket="${GCP_BUCKET}" --database="${DATABASE_INSTANCE}" 
 else
   echo "Invalid Test Suite"
   exit 1
 fi
 
-echo "All Tests Passed Successfully"
+echo "${TEST_SUITE} Tests Passed Successfully"
 exit 0
