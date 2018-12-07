cf2336af8771c16274c1113ee72ed7fe8b1bca5a
Christopher Drake <drake@cdrake.net>
2018-12-06 17:12:15 -0800

    Mysql util (#154)
    * Restructured directories for namespace purposes. Added mysql_util for performing MySQLDump to Cloud Storage.
    
    * Changed import in __init__ to use __import__ from import. Changed text in __init__ for replicator/util.

--- google/cloud/pontem/sql/replicator/util/util_errors.py
+++ google/cloud/pontem/sql/replicator/util/util_errors.py
@@ -12,4 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Pontem Cloud SQL Replicator Setup Util"""
+"""Errors associated with SQL Replicator Util package."""
+class MySQLDumpError(Exception)
