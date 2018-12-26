b69368fa0e2a0eb5778361b9eff6c1edf4b94143
cdrake <chrisdrake@google.com>
2018-12-26 12:55:44 -0800

    Fixed white space in __init__ files.

--- CloudSQLReplicator/tests/util/compute_test.py
+++ CloudSQLReplicator/tests/util/compute_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests GCP API utility functions."""
+"""Tests GCP API Compute utility functions."""
 
 import unittest
 import mock
