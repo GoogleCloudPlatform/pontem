0de0d0a134697c7d3db9dbae7cbfe57e6572f514
Lucas Daniel Madariaga <lmadariaga@google.com>
2019-07-01 14:59:31 -0700

    Merge dev into master.

---
+++ BigQueryWorkloadTester/src/test/java/com/google/cloud/pontem/benchmark/RatioBasedWorkloadBenchmarkTest.java
@@ -0,0 +1,261 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.pontem.benchmark;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.AdditionalAnswers.returnsElementsOf;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.google.cloud.pontem.benchmark.runners.ConcurrentWorkloadRunner;
+import com.google.cloud.pontem.benchmark.runners.ConcurrentWorkloadRunnerFactory;
+import com.google.cloud.pontem.config.WorkloadSettings;
+import com.google.cloud.pontem.model.WorkloadResult;
+import com.google.cloud.pontem.testing.MapBuilder;
+import com.google.cloud.pontem.testing.TestConstants;
+import com.google.cloud.pontem.testing.WorkloadResultHelper;
+import com.google.cloud.pontem.testing.WorkloadSettingsHelper;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Tests for {@link RatioBasedWorkloadBenchmark}. */
+@RunWith(JUnit4.class)
+public class RatioBasedWorkloadBenchmarkTest {
+  private static final List<Double> BENCHMARK_RATIOS =
+      Arrays.asList(0.01, 0.1, 0.25, 0.5, 1.0, 1.50, 2.0);
+
+  private ConcurrentWorkloadRunnerFactory concurrentWorkloadRunnerFactoryMock;
+
+  private RatioBasedWorkloadBenchmark ratioBasedWorkloadBenchmark;
+
+  @Before
+  public void setUp() {
+    this.concurrentWorkloadRunnerFactoryMock = mock(ConcurrentWorkloadRunnerFactory.class);
+
+    this.ratioBasedWorkloadBenchmark =
+        new RatioBasedWorkloadBenchmark(concurrentWorkloadRunnerFactoryMock, BENCHMARK_RATIOS);
+  }
+
+  @Test
+  public void runsBenchmarkWithAllPercentages() {
+    int concurrencyLevel = 15;
+    WorkloadSettings workload = WorkloadSettingsHelper.getMultipleQueries();
+
+    List<Integer> expectedConcurrencyLevels = new ArrayList<>();
+    for (double percentage : BENCHMARK_RATIOS) {
+      int expectedConcurrencyLevel = (int) Math.ceil(concurrencyLevel * percentage);
+      expectedConcurrencyLevels.add(expectedConcurrencyLevel);
+    }
+
+    List<Integer> concurrencyLimits = new ArrayList<>();
+    List<ConcurrentWorkloadRunner> concurrentWorkloadRunnerMocks = new ArrayList<>();
+    List<List<WorkloadResult>> workloadResultList = new ArrayList<>();
+    for (Integer expectedConcurrencyLevel : expectedConcurrencyLevels) {
+      int concurrencyLimit =
+          Math.min(expectedConcurrencyLevel, TestConstants.CONCURRENCY_LEVEL_LIMIT);
+      List<WorkloadResult> results =
+          WorkloadResultHelper.getWorkloadResultsForConcurrencyLevel(expectedConcurrencyLevel);
+
+      concurrencyLimits.add(concurrencyLimit);
+      concurrentWorkloadRunnerMocks.add(mock(ConcurrentWorkloadRunner.class));
+      workloadResultList.add(results);
+    }
+
+    Map<ConcurrentWorkloadRunner, List<WorkloadResult>> mocksAndResults =
+        MapBuilder.getImmutableMapFromLists(concurrentWorkloadRunnerMocks, workloadResultList);
+
+    when(concurrentWorkloadRunnerFactoryMock.getConcurrentWorkloadRunner(
+            any(WorkloadSettings.class), anyInt()))
+        .thenAnswer(returnsElementsOf(concurrentWorkloadRunnerMocks));
+    when(concurrentWorkloadRunnerFactoryMock.getConcurrencyLimit(anyInt()))
+        .thenAnswer(returnsElementsOf(concurrencyLimits));
+    for (Entry<ConcurrentWorkloadRunner, List<WorkloadResult>> entry : mocksAndResults.entrySet()) {
+      when(entry.getKey().run(any(WorkloadSettings.class), anyInt())).thenReturn(entry.getValue());
+    }
+
+    List<WorkloadResult> workloadResults =
+        ratioBasedWorkloadBenchmark.run(workload, concurrencyLevel);
+
+    List<WorkloadResult> expectedWorkloadResults =
+        workloadResultList.stream().flatMap(List::stream).collect(Collectors.toList());
+    assertThat(workloadResults, is(expectedWorkloadResults));
+
+    for (ConcurrentWorkloadRunner concurrentWorkloadRunnerMock : concurrentWorkloadRunnerMocks) {
+      verify(concurrentWorkloadRunnerMock, times(1)).run(any(WorkloadSettings.class), anyInt());
+    }
+  }
+
+  @Test
+  public void runsBenchmarkUntilLimitIsReached() {
+    int concurrencyLevel = 90;
+    WorkloadSettings workload = WorkloadSettingsHelper.getMultipleQueries();
+
+    List<Integer> expectedConcurrencyLevels = new ArrayList<>();
+    for (double percentage : BENCHMARK_RATIOS.subList(0, 4)) {
+      int expectedConcurrencyLevel = (int) Math.ceil(concurrencyLevel * percentage);
+      expectedConcurrencyLevels.add(expectedConcurrencyLevel);
+    }
+    expectedConcurrencyLevels.add(TestConstants.CONCURRENCY_LEVEL_LIMIT);
+
+    List<Integer> concurrencyLimits = new ArrayList<>();
+    List<ConcurrentWorkloadRunner> concurrentWorkloadRunnerMocks = new ArrayList<>();
+    List<List<WorkloadResult>> workloadResultList = new ArrayList<>();
+    for (Integer expectedConcurrencyLevel : expectedConcurrencyLevels) {
+      int concurrencyLimit =
+          Math.min(expectedConcurrencyLevel, TestConstants.CONCURRENCY_LEVEL_LIMIT);
+      List<WorkloadResult> results =
+          WorkloadResultHelper.getWorkloadResultsForConcurrencyLevel(expectedConcurrencyLevel);
+
+      concurrencyLimits.add(concurrencyLimit);
+      concurrentWorkloadRunnerMocks.add(mock(ConcurrentWorkloadRunner.class));
+      workloadResultList.add(results);
+    }
+
+    Map<ConcurrentWorkloadRunner, List<WorkloadResult>> mocksAndResults =
+        MapBuilder.getImmutableMapFromLists(concurrentWorkloadRunnerMocks, workloadResultList);
+
+    when(concurrentWorkloadRunnerFactoryMock.getConcurrentWorkloadRunner(
+            any(WorkloadSettings.class), anyInt()))
+        .thenAnswer(returnsElementsOf(concurrentWorkloadRunnerMocks));
+    when(concurrentWorkloadRunnerFactoryMock.getConcurrencyLimit(anyInt()))
+        .thenAnswer(returnsElementsOf(concurrencyLimits));
+    for (Entry<ConcurrentWorkloadRunner, List<WorkloadResult>> entry : mocksAndResults.entrySet()) {
+      when(entry.getKey().run(any(WorkloadSettings.class), anyInt())).thenReturn(entry.getValue());
+    }
+
+    List<WorkloadResult> workloadResults =
+        ratioBasedWorkloadBenchmark.run(workload, concurrencyLevel);
+
+    List<WorkloadResult> expectedWorkloadResults =
+        workloadResultList.stream().flatMap(List::stream).collect(Collectors.toList());
+    assertThat(workloadResults, is(expectedWorkloadResults));
+
+    for (ConcurrentWorkloadRunner concurrentWorkloadRunnerMock : concurrentWorkloadRunnerMocks) {
+      verify(concurrentWorkloadRunnerMock, times(1)).run(any(WorkloadSettings.class), anyInt());
+    }
+  }
+
+  @Test
+  public void runsBenchmarkIgnoringRepeatedLevels() {
+    int concurrencyLevel = 4;
+    WorkloadSettings workload = WorkloadSettingsHelper.getMultipleQueries();
+
+    List<Integer> expectedConcurrencyLevels = new ArrayList<>();
+    for (double percentage : BENCHMARK_RATIOS.subList(2, 7)) {
+      int expectedConcurrencyLevel = (int) Math.ceil(concurrencyLevel * percentage);
+      expectedConcurrencyLevels.add(expectedConcurrencyLevel);
+    }
+
+    List<Integer> concurrencyLimits = new ArrayList<>();
+    List<ConcurrentWorkloadRunner> concurrentWorkloadRunnerMocks = new ArrayList<>();
+    List<List<WorkloadResult>> workloadResultList = new ArrayList<>();
+    for (Integer expectedConcurrencyLevel : expectedConcurrencyLevels) {
+      int concurrencyLimit =
+          Math.min(expectedConcurrencyLevel, TestConstants.CONCURRENCY_LEVEL_LIMIT);
+      List<WorkloadResult> results =
+          WorkloadResultHelper.getWorkloadResultsForConcurrencyLevel(expectedConcurrencyLevel);
+
+      concurrencyLimits.add(concurrencyLimit);
+      concurrentWorkloadRunnerMocks.add(mock(ConcurrentWorkloadRunner.class));
+      workloadResultList.add(results);
+    }
+
+    Map<ConcurrentWorkloadRunner, List<WorkloadResult>> mocksAndResults =
+        MapBuilder.getImmutableMapFromLists(concurrentWorkloadRunnerMocks, workloadResultList);
+
+    when(concurrentWorkloadRunnerFactoryMock.getConcurrentWorkloadRunner(
+            any(WorkloadSettings.class), anyInt()))
+        .thenAnswer(returnsElementsOf(concurrentWorkloadRunnerMocks));
+    when(concurrentWorkloadRunnerFactoryMock.getConcurrencyLimit(anyInt()))
+        .thenAnswer(returnsElementsOf(concurrencyLimits));
+    for (Entry<ConcurrentWorkloadRunner, List<WorkloadResult>> entry : mocksAndResults.entrySet()) {
+      when(entry.getKey().run(any(WorkloadSettings.class), anyInt())).thenReturn(entry.getValue());
+    }
+
+    List<WorkloadResult> workloadResults =
+        ratioBasedWorkloadBenchmark.run(workload, concurrencyLevel);
+
+    List<WorkloadResult> expectedWorkloadResults =
+        workloadResultList.stream().flatMap(List::stream).collect(Collectors.toList());
+    assertThat(workloadResults, is(expectedWorkloadResults));
+
+    for (ConcurrentWorkloadRunner concurrentWorkloadRunnerMock : concurrentWorkloadRunnerMocks) {
+      verify(concurrentWorkloadRunnerMock, times(1)).run(any(WorkloadSettings.class), anyInt());
+    }
+  }
+
+  @Test
+  public void onlyRunsBenchmarkAtTheConcurrencyLimit() {
+    int concurrencyLevel = 9999;
+    int concurrencyLimit = TestConstants.CONCURRENCY_LEVEL_LIMIT;
+
+    WorkloadSettings workload = WorkloadSettingsHelper.getMultipleQueries();
+    List<WorkloadResult> workloadResultsForLimit =
+        WorkloadResultHelper.getWorkloadResultsForConcurrencyLevel(concurrencyLimit);
+    ConcurrentWorkloadRunner concurrentWorkloadRunnerMock = mock(ConcurrentWorkloadRunner.class);
+
+    when(concurrentWorkloadRunnerFactoryMock.getConcurrentWorkloadRunner(
+            any(WorkloadSettings.class), anyInt()))
+        .thenReturn(concurrentWorkloadRunnerMock);
+    when(concurrentWorkloadRunnerFactoryMock.getConcurrencyLimit(anyInt()))
+        .thenReturn(concurrencyLimit);
+    when(concurrentWorkloadRunnerMock.run(any(WorkloadSettings.class), anyInt()))
+        .thenReturn(workloadResultsForLimit);
+
+    List<WorkloadResult> workloadResults =
+        ratioBasedWorkloadBenchmark.run(workload, concurrencyLevel);
+
+    assertThat(workloadResults.size(), is(1));
+    assertThat(workloadResults, is(workloadResultsForLimit));
+
+    verify(concurrentWorkloadRunnerMock, times(1)).run(workload, concurrencyLimit);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void failsForNullBenchmarkRatios() {
+    int concurrencyLevel = 15;
+    WorkloadSettings workload = WorkloadSettingsHelper.getMultipleQueries();
+
+    RatioBasedWorkloadBenchmark ratioBasedWorkloadBenchmark =
+        new RatioBasedWorkloadBenchmark(concurrentWorkloadRunnerFactoryMock, null);
+
+    ratioBasedWorkloadBenchmark.run(workload, concurrencyLevel);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void failsForEmptyBenchmarkRatios() {
+    int concurrencyLevel = 15;
+    WorkloadSettings workload = WorkloadSettingsHelper.getMultipleQueries();
+
+    RatioBasedWorkloadBenchmark ratioBasedWorkloadBenchmark =
+        new RatioBasedWorkloadBenchmark(concurrentWorkloadRunnerFactoryMock, Arrays.asList());
+
+    ratioBasedWorkloadBenchmark.run(workload, concurrencyLevel);
+  }
+}
---
+++ BigQueryWorkloadTester/src/test/java/com/google/cloud/pontem/benchmark/runners/callables/WorkloadRunnerCallableTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.pontem.benchmark.runners.callables;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.google.cloud.pontem.benchmark.runners.SerialQueryRunner;
+import com.google.cloud.pontem.config.WorkloadSettings;
+import com.google.cloud.pontem.model.WorkloadResult;
+import com.google.cloud.pontem.testing.QueryResultHelper;
+import com.google.cloud.pontem.testing.TestConstants;
+import com.google.cloud.pontem.testing.WorkloadResultHelper;
+import com.google.cloud.pontem.testing.WorkloadSettingsHelper;
+import com.google.common.base.Stopwatch;
+import com.google.common.testing.FakeTicker;
+import java.util.concurrent.TimeUnit;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Tests for {@link WorkloadRunnerCallable}. */
+@RunWith(JUnit4.class)
+public class WorkloadRunnerCallableTest {
+
+  private SerialQueryRunner serialQueryRunnerMock;
+  private WorkloadSettings workloadMock;
+  private FakeTicker fakeTicker;
+  private Stopwatch stopwatch;
+
+  private WorkloadRunnerCallable workloadRunnerCallable;
+
+  @Before
+  public void setUp() {
+    this.serialQueryRunnerMock = mock(SerialQueryRunner.class);
+    this.workloadMock = WorkloadSettingsHelper.getMultipleQueries();
+    this.fakeTicker = new FakeTicker();
+    this.stopwatch = Stopwatch.createUnstarted(fakeTicker);
+
+    this.workloadRunnerCallable =
+        new WorkloadRunnerCallable(
+            serialQueryRunnerMock,
+            stopwatch,
+            workloadMock,
+            TestConstants.CONCURRENCY_LEVEL_BELOW_LIMIT);
+  }
+
+  @Test
+  public void getAndRunSuccessfully() {
+    fakeTicker.setAutoIncrementStep(
+        TestConstants.SUCCESSFUL_WORKLOAD_WALLTIME, TimeUnit.MILLISECONDS);
+    when(serialQueryRunnerMock.run(TestConstants.TEST_QUERIES))
+        .thenReturn(QueryResultHelper.getSuccessfulQueryResults());
+
+    WorkloadResult workloadResult = workloadRunnerCallable.call();
+
+    assertThat(
+        workloadResult, is(WorkloadResultHelper.getWorkloadResultMultipleSuccessfulQueries()));
+    verify(serialQueryRunnerMock).run(TestConstants.TEST_QUERIES);
+  }
+
+  @Test
+  public void getAndRunWithQueryErrors() {
+    fakeTicker.setAutoIncrementStep(TestConstants.FAILING_WORKLOAD_WALLTIME, TimeUnit.MILLISECONDS);
+    when(serialQueryRunnerMock.run(TestConstants.TEST_QUERIES))
+        .thenReturn(QueryResultHelper.getQueryResultsWithSingleError());
+
+    WorkloadResult workloadResult = workloadRunnerCallable.call();
+
+    assertThat(workloadResult, is(WorkloadResultHelper.getWorkloadResultMultipleQueryErrors()));
+    verify(serialQueryRunnerMock).run(TestConstants.TEST_QUERIES);
+  }
+}
